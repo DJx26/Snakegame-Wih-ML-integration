261f4fbf8e72c29ebfaec97cf1328ae8056e34ca
DJx26 <dakshpersonal11@gmail.com>
2025-07-25 21:43:31 +0530

    Update required libs.m

--- required libs.m
+++ required libs.m
@@ -3,8 +3,8 @@ import tensorflow as tf
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
-import pygame
 
+import pygame
 import random
 import ipywidgets as widgets
 from IPython.display import display
